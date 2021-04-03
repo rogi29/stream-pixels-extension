5f76e13e6d6fae45052775e42c332884096b345f
Gil Nimer <gil.nimer@lognet-systems.com>
2021-04-03 14:26:43 +0300

    Simulate stream with youtube iframe, should be deleted before depolyment or test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import './App.css';
 import { SWRConfig } from 'swr';
-import CharacterScene from './components/scenes/CharacterScene';
+import { UserDataProvider } from 'components/contexts/UserDataContext';
+import CharacterCustomizationScreen from 'components/screens/CharacterCustomizationScreen';
 
 const swrConfig = {
     revalidateOnFocus: false
@@ -7,9 +9,21 @@ const swrConfig = {
 
 const App = () => (
     <SWRConfig value={swrConfig}>
-        <div className="App" style={{ height: '100vh', background: '#aaa' }}>
-            <CharacterScene />
-        </div>
+        <UserDataProvider id="0">
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/388wUjNHSGE?controls=0&loop=1&autoplay=1&showinfo=0&loop=1&mute=1&amp;start=15"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                style={{ position: 'absolute' }}
+            ></iframe>
+            <div className="App">
+                <CharacterCustomizationScreen />
+            </div>
+        </UserDataProvider>
     </SWRConfig>
 );
 
